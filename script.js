d4e662415dc28009e1b38105c18917cc3445c23a
Alone-07 <al71one2002@gmail.com>
2023-02-26 16:26:54 +0530

    home still not completed

--- script.js
+++ script.js
@@ -48,7 +48,78 @@ btnContainer.addEventListener("click", (e) => {
   target.classList.add("clicked");
 
   if (targetedCard) {
-    mainCard.forEach((elem) => (elem.style.display = "none"));
-    targetedCard.style.display = "block";
+    mainCard.forEach((elem) => {
+      if (elem != targetedCard) {
+        // elem.style.display = "none";
+        elem.classList.add("none");
+        elem.classList.remove("block");
+      }
+    });
+    if (!targetedCard.classList.contains("block")) {
+      targetedCard.classList.remove("none");
+      targetedCard.classList.add("block");
+    } else {
+      targetedCard.classList.remove("block");
+      targetedCard.classList.add("none");
+      target.classList.remove("clicked");
+    }
+    // targetedCard.classList.toggle("block");
   }
+});
+
+let file = document.querySelector("#uploadFile");
+let uploadConfirmCard = document.querySelector(".uploadConfirmCard");
+let uploadCard = document.querySelector(".uploadCard");
+let uploadConfirmSubmit = document.querySelector(".uploadConfirmSubmit");
+let uploadFile = document.querySelector(".uploadFile");
+let uploadFileName = document.querySelector(".uploadFileName");
+
+file.addEventListener("change", (e) => {
+  let value = file.value;
+  let slashIndex = value.lastIndexOf("\\");
+  let trim = value.substring(slashIndex + 1);
+  uploadFileName.innerText = trim;
+});
+
+uploadConfirmSubmit.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (!groupName.value || !groupPassword.value)
+    return alert("fill all the inputs");
+  uploadConfirmCard.style.display = "none";
+  uploadCard.style.display = "flex";
+});
+uploadFile.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (!uploadFileName.innerText) return alert("atleast choose 1 file!!");
+  uploadFileName.innerText = "";
+  uploadCard.parentElement.classList.toggle("block");
+});
+
+let loginSignupSubmit = document.querySelectorAll(
+  '.loginSignup button[type="submit"]'
+);
+
+loginSignupSubmit.forEach((elem) => {
+  let formContainerInput = document.querySelectorAll(".formContainer > input");
+  elem.addEventListener("click", (e) => {
+    e.preventDefault();
+    e.target.parentElement.parentElement.classList.add("none");
+    return formContainerInput.forEach((elem) => (elem.value = ""));
+  });
+});
+
+let downloadConfirmSubmit = document.querySelector(".downloadConfirmSubmit");
+let downloadFileSubmit = document.querySelector(".downloadFileSubmit");
+let downloadConfirmCard = document.querySelector(".downloadConfirmCard");
+let downloadCard = document.querySelector(".downloadCard");
+
+downloadConfirmSubmit.addEventListener("click", (e) => {
+  e.preventDefault();
+  downloadConfirmCard.style.display = "none";
+  downloadCard.style.display = "flex";
+});
+
+downloadCard.addEventListener("click", (e) => {
+  e.preventDefault();
+  downloadCard.parentElement.classList.toggle("block");
 });
